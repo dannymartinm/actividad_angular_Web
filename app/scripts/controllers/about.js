14b8ccdf1be8227dcdb22c8515a517107a900102
Daniela Martin <daniela@esporaestudio.com>
2017-02-20 16:31:57 -0600

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectoAngularApp
+ */
+angular.module('proyectoAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proyectoAngularApp
+ */
+angular.module('proyectoAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
