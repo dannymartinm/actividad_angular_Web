c41bd4f60bba584db0de6f750318b4e3683bdc31
Daniela Martin <daniela@esporaestudio.com>
2017-02-22 09:35:50 -0600

    Ya se pueden añadir libros.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,17 @@
  */
 angular.module('proyectoAngularApp')
   .controller('MainCtrl', function ($scope) {
-    
+    var modelo={
+    	libro : [{autor: "J.K", titulo: "Harry Potter", isbn: "123456790",
+    			editorial : "Anagrama", anioP: "2005", numEd: "2",
+    			pasta: "dura", costo: "$300", proveedor : "Proveedor1"}]
+    };
+    $scope.modelo = modelo;
+
+    $scope.addBook = function(autorT, tituloT, isbnT, editorialT, anioPT, numEdT, pastaT, costoT, proveedorT) {
+  		$scope.modelo.libro.push({autor:autorT, titulo : tituloT, isbn : isbnT,
+  									editorial : editorialT, anioP : anioPT, numEd : numEdT,
+  									pasta : pastaT, costo : costoT, proveedor : proveedorT});
+  	}
+
   });
