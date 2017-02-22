4bae718e45dcd7f9a32ce633d26db881ee129345
Daniela Martin <daniela@esporaestudio.com>
2017-02-22 08:54:48 -0600

    Se modificó el html.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,6 @@
  * Controller of the proyectoAngularApp
  */
 angular.module('proyectoAngularApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    
   });
