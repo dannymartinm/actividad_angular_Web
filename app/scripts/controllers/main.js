591ea2b1d239cb6e736feb642e719e615301e866
Daniela Martin <daniela@esporaestudio.com>
2017-02-22 09:44:30 -0600

    Se agregaron los campos: fecha, usuario y estatus. Pendiente sacar fecha bien

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,17 +9,19 @@
  */
 angular.module('proyectoAngularApp')
   .controller('MainCtrl', function ($scope) {
+  	
     var modelo={
-    	libro : [{autor: "J.K", titulo: "Harry Potter", isbn: "123456790",
+    	libro : [{fecha: "22/02/2017", usuario: "Gandhi", autor: "J.K", titulo: "Harry Potter", isbn: "123456790",
     			editorial : "Anagrama", anioP: "2005", numEd: "2",
-    			pasta: "dura", costo: "$300", proveedor : "Proveedor1"}]
+    			pasta: "dura", costo: "$300", proveedor : "Proveedor1", estatus : "Pendiente"}]
     };
     $scope.modelo = modelo;
 
     $scope.addBook = function(autorT, tituloT, isbnT, editorialT, anioPT, numEdT, pastaT, costoT, proveedorT) {
-  		$scope.modelo.libro.push({autor:autorT, titulo : tituloT, isbn : isbnT,
+    	var fecha = Date.now();
+  		$scope.modelo.libro.push({fecha : fecha, usuario : "usuario1", autor:autorT, titulo : tituloT, isbn : isbnT,
   									editorial : editorialT, anioP : anioPT, numEd : numEdT,
-  									pasta : pastaT, costo : costoT, proveedor : proveedorT});
+  									pasta : pastaT, costo : costoT, proveedor : proveedorT, estatus : "Pendiente"});
   	}
 
   });
