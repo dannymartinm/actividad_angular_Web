384ca43ece9d549bce95ae0bdfb911d1ce09a1f3
Daniela Martin <daniela@esporaestudio.com>
2017-02-22 18:42:45 -0600

    Ya se ve correctamente la fecha.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,16 +11,24 @@ angular.module('proyectoAngularApp')
   .controller('MainCtrl', function ($scope) {
   	
     var modelo={
-    	libro : [{fecha: "22/02/2017", usuario: "Gandhi", autor: "J.K", titulo: "Harry Potter", isbn: "123456790",
+    	libro : [{fecha: "22/2/2017", usuario: "Gandhi", autor: "J.K", titulo: "Harry Potter", isbn: "123456790",
     			editorial : "Anagrama", anioP: "2005", numEd: "2",
     			pasta: "dura", costo: "$300", proveedor : "Proveedor1", estatus : "Pendiente"}]
     };
     $scope.modelo = modelo;
 
     $scope.addBook = function(autorT, tituloT, isbnT, editorialT, anioPT, numEdT, pastaT, costoT, proveedorT) {
-    	var fecha = Date.now();
+    	var tmp = new Date();
+  		var day = tmp.getDate();
+  		var month = tmp.getMonth()+1;
+  		var year = tmp.getFullYear();
+
+  		var fecha = day+"/"+month+"/"+year;
+
+
+
     	var user = Math.floor(Math.random() * 6) + 1  
-  		$scope.modelo.libro.push({fecha : fecha, usuario : ("usuario "+user), autor:autorT, titulo : tituloT, isbn : isbnT,
+  		$scope.modelo.libro.push({fecha : fecha, usuario : ("usuario"+user), autor:autorT, titulo : tituloT, isbn : isbnT,
   									editorial : editorialT, anioP : anioPT, numEd : numEdT,
   									pasta : pastaT, costo : costoT, proveedor : proveedorT, estatus : "Pendiente"});
   	}
